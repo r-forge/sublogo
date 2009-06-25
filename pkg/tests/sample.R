951840d6264272e8940154c13e2ddeb575a8461d
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2009-06-25 16:40:00 +0000

    perfect simulated example fabricated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sublogo@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/sample.R
+++ pkg/tests/sample.R
@@ -23,3 +23,41 @@ sublogo(seqs,cutline=10)
 ## conditional probability link between positions i and j, simulate i
 ## according to substitution matrix, simulate j according to
 ## probability conditional on i, then add noise
+good.rows <- function(x)rowSums(x>0.4)==2
+rmat <- function(N=10){
+  sm <- matrix(0)
+  ## this generates an interesting starting substitution matrix, where
+  ## there are at least 2 positions with freq>40%
+  while(sum(gr <- good.rows(sm))<2){
+    x <- matrix(rexp(N*4),N,4)
+    sm <- x/rowSums(x)
+  }
+  print(cbind(gr,sm))
+  colnames(sm) <- dna.letters[2:5]
+  sm
+}
+rseq <- function(n,m,corr=0){
+  lmat <- apply(m,1,function(p)sample(names(p),n,T,p))
+  if(corr){
+    good <- which(good.rows(m))
+    p1 <- m[good[1],]
+    g1 <- names(which(p1>0.4))
+    p2 <- m[good[2],]
+    g2 <- names(which(p2>0.4))
+    favor <- function(l){
+      i <- which(l==g1)
+      if(length(i)==0)return(c(1,1,1,1))
+      favored <- names(p2)==g2[i]
+      (1-corr)/3*(!favored) + corr*favored
+    }
+    y <- t(sapply(lmat[,good[1]],favor))
+    norm <- y/rowSums(y) ## easier to look at/compare
+    lmat[,good[2]] <- apply(norm,1,function(v)sample(dna.letters[2:5],1,prob=v))    
+  }
+  apply(lmat,1,paste,collapse='')
+}
+##debug(rseq)
+m <- rmat() ## make a substitution matrix
+seqs <- rseq(50,m,0.95) ## randomly draw some sequences from that matrix
+sublogo(seqs,cutline=6.7)
+t(cbind(m,good.rows(m)))
