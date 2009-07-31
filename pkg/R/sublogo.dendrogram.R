692f552ac9e4c0d44ae51b0bbfdb1859d9946a33
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2009-07-31 16:25:49 +0000

    doc generation beginning to work, still need to debug data set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sublogo@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sublogo.dendrogram.R
+++ pkg/R/sublogo.dendrogram.R
@@ -13,17 +13,24 @@ read.fasta <- function
 }
 ##debug(read.fasta)
 
+### Letters in the DNA alphabet, used to auto-detect sequence type
 dna.letters <- c("*","A","T","G","C")
+### DNA identity substitution matrix.
 dna.identity <- matrix(0,nrow=length(dna.letters),ncol=length(dna.letters),
                        dimnames=list(dna.letters,dna.letters))
 diag(dna.identity) <- 1
 dna.identity['*','*'] <- 0
 seqs.to.mat <- function
-### calculate pairwise differences between sequences using a substitution matrix
+### Calculate pairwise differences between sequences using a
+### substitution matrix.
 (seq.vec,
-### dna or protein sequences
+### DNA or protein sequences.
  subs.mat=NULL){
-### substitution matrix with dimnames that match the letters used in the sequence data
+### Substitution matrix with dimnames that match the letters used in
+### the sequence data, or a character vector that specifies a common
+### substitution matrix (as defined in the Biostrings package). NULL
+### specifies that we will guess a suitable substitution matrix to
+### match your input sequences (DNA=>identity, protein=>BLOSUM62).
   if(is.null(names(seq.vec)))names(seq.vec) <- seq.vec
   chars <- sapply(seq.vec,nchar)
   seqsum <- table(chars)
@@ -63,16 +70,29 @@ seqs.to.mat <- function
   m <- m-min(m)
   attr(m,'seqs') <- d
   m
+### The matrix of distances between each input sequence, with dimnames
+### corresponding to either the sequences, or the sequence names (if
+### they exist)
 }
 ##debug(seqs.to.mat)
 
-## vector format reading available with grImport + R>=2.3
-make.logo.ps <- function(helices,psbase){
+make.logo.ps <- function
+### Create a logo using weblogo, then read it in using grImport
+(helices,
+### Sequences to plot in the logo
+ psbase
+### Base filename for the logo postscript and xml files, should be the
+### full path name except for the trailing .ps
+ ){
   psfile <- paste(psbase,'ps',sep='.')
   xmlfile <- paste(psfile,'xml',sep='.')
   seq.text <- paste(paste('>',helices,'\n',helices,sep=''),collapse='\n')
   write(seq.text,psbase)
-  cmd <- paste("PATH=/home/thocking/Desktop/sublogo/pkg/exec:$PATH seqlogo -c -F EPS -f",psbase,"|sed 's/^EndLine/%EndLine/'|sed 's/^EndLogo/%EndLogo/' >",psfile)
+  execdir <- system.file("exec",package="sublogo")
+  cmd <- paste("PATH=",execdir,
+               ":$PATH seqlogo -c -F EPS -f ",psbase,
+               "|sed 's/^EndLine/%EndLine/'|sed 's/^EndLogo/%EndLogo/' >",
+               psfile,sep="")
   cat(cmd,'\n')
   system(cmd)
   owd <- setwd(tempdir())
@@ -80,13 +100,32 @@ make.logo.ps <- function(helices,psbase){
   setwd(owd)
   pic <- readPicture(xmlfile)
   pic
+### Grid picture grob as read using readPicture
 }
 ##debug(make.logo.ps)
 
-subtitle <- function(st) mtext(st,line=-0.7,cex=1)
-sublogo.dendrogram <- function(
-  M,main='',subtit=NULL,base=NULL,cutline=150,dend.width=30,cex=1){
-  if(is.null(base))base <- tempfile()
+sublogo.dendrogram <- function
+### Main function for drawing sublogo dendrograms.
+(M,
+### difference matrix as constructed by seqs.to.mat (although in
+### principle any object with a valid as.dist method could be used)
+ main='',
+### plot title
+ subtit=NULL,
+### plot subtitle
+ base=tempfile(),
+### base file name for temporary logo files
+ cutline=150,
+### Distance for cutting the tree. Draw a sublogo for each
+### leaf. Normally you will plot once, then inspect the dendrogram to
+### determine which is a good value for cutline, then plot again using
+### your chosen cutline.
+ dend.width=30,
+### Percent of the plot to be occupied by the dendrogram. The logos
+### will be displayed with equal widths on either side.
+ cex=1
+### character expansion factor for the dendrogram
+ ){
   hc <- hclust(as.dist(M),method="average")
   dend <- as.dendrogram(hc)
   fam <- cutree(hc,h=cutline)[labels(dend)] # order by plotting method
@@ -115,23 +154,21 @@ sublogo.dendrogram <- function(
                    x=xrange[1],
                    y=unit(grprange[1]+0.25,'native'),
                    height=unit(diff(grprange)-0.5,'native'),
-                   distort=T,
+                   distort=TRUE,
                    just=c(0,0),
-                   fillText=T)
+                   fillText=TRUE)
     }
   }
 
   bottomspace <- 0.6
   topspace <- 0.4
-  ncex <- 1
   side.percents <- (100-dend.width)/2
   layout(matrix(1:3,ncol=3),c(side.percents,dend.width,side.percents))
-  par(mai=c(bottomspace,0,topspace,0),cex=ncex)
+  par(mai=c(bottomspace,0,topspace,0),cex=1)
   
   ## Big summary logo on left
   plot(xrange,yrange,
        bty='n',xaxt='n',yaxt='n',ylab='',xlab='',type='n',yaxs='i')
-  ##subtitle("Overall logo")
   biglogo <- make.logo.ps(famtab$seq,paste(base,'0',sep='.'))
   vps <- baseViewports()
   pushViewport(vps$inner,vps$figure,vps$plot)
@@ -140,8 +177,8 @@ sublogo.dendrogram <- function(
                height=unit(length(fam),'native'),
                just=c(0.5,0),
                exp=0,
-               distort=T,
-               fillText=T)
+               distort=TRUE,
+               fillText=TRUE)
   popViewport(3)
   
   ## Dendrogram in middle
@@ -150,29 +187,39 @@ sublogo.dendrogram <- function(
                      cex))/6*5),
       family='mono')
   par(xpd=NA)
-  plot(dend,h=T,edgePar=list(lwd=2),nodePar=list(lab.cex=cex,pch=""))
+  plot(dend,h=TRUE,edgePar=list(lwd=2),nodePar=list(lab.cex=cex,pch=""))
   par(family="")
   segments(cutline,1,cutline,length(fam))
   ##axis(3,cutline,lty=0,line=0)
   
   ## Title in the middle
   title(main,line=0.5)
-  ##subtitle("Dendrogram")
-  subtitle(subtit)
+  mtext(subtit,line=-0.7,cex=1)
 
   # Sublogos on the right side
   par(mai=c(bottomspace,0,topspace,0))
   plot(xrange,yrange,
        bty='n',xaxt='n',yaxt='n',ylab='',xlab='',type='n',yaxs='i')
-  ##subtitle("Sublogos")
   vps <- baseViewports()
   pushViewport(vps$inner,vps$figure,vps$plot)
   sapply(famids,draw.logo)
   popViewport(3)
   
   hc
+### The dendrogram from the call to hclust
 }
 ##debug(sublogo.dendrogram)
 
-## shortcut function for common case of sequence data
-sublogo <- function(seqs,mat=NULL,...)sublogo.dendrogram(seqs.to.mat(seqs,mat),...)
+sublogo <- function
+### Draw a sublogo dendrogram for a sequence motif.
+(seqs,
+### Character vector of DNA or protein sequences (optionally named
+### with sequence labels).
+ mat=NULL,
+### Substitution matrix passed to seqs.to.mat.
+ ...
+### Other arguments to pass to sublogo.dendrogram (see that help page
+### for a full description).
+ ){
+  sublogo.dendrogram(seqs.to.mat(seqs,mat),...)
+}
